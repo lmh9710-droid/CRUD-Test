15bf3909c6fcb317c597fe80825f68a8b6d94446
Autumn_Light <lmh9710@gmail.com>
2026-06-29 20:51:29 +0900

    Add files via upload

---
+++ CRUD.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void create_vendor();
+
+void read_vendor();
+
+void update_vendor();
+
+void delete_vendor();
---
+++ vendor_t.h
@@ -0,0 +1,12 @@
+#pragma once
+
+typedef struct
+{
+	int id;
+	char vendor[80];
+	char name[30];
+	char position[20];
+	char phone[20];
+
+}Vendor;
+
